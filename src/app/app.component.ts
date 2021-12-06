a764278b9de8b113383bab64df80cb79dbf6afe4
st180 <shaun.teeshad@gmail.com>
2021-12-06 12:31:42 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './navbar-top-fixed.css']
 })
 export class AppComponent {
   title = 'qr-now';
